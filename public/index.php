46e4ffe661cf0b22496f2b756c3efac0ba8d1c67
Phillip Alisson Volpi <phill.volpi@gmail.com>
2026-02-03 19:56:20 -0300

    feat: middleware CORS para frontend em desenvolvimento
    - Permite origens localhost:3000, 3001 e 127.0.0.1
    - Responde OPTIONS com headers Allow-Methods e Allow-Headers
    - Inclui Content-Type e Authorization nas requisições

--- public/index.php
+++ public/index.php
@@ -18,6 +18,34 @@ $container = require __DIR__ . '/../config/container.php';
 AppFactory::setContainer($container);
 
 $app = AppFactory::create();
+
+// CORS middleware – permitir frontend em desenvolvimento
+$app->add(function ($request, $handler) {
+    $origin = $request->getHeaderLine('Origin') ?: '*';
+    $allowedOrigins = ['http://localhost:3000', 'http://localhost:3001', 'http://127.0.0.1:3000', 'http://127.0.0.1:3001'];
+    if (in_array($origin, $allowedOrigins, true)) {
+        $responseOrigin = $origin;
+    } else {
+        $responseOrigin = '*';
+    }
+
+    if ($request->getMethod() === 'OPTIONS') {
+        $response = new \Slim\Psr7\Response();
+        $response = $response
+            ->withHeader('Access-Control-Allow-Origin', $responseOrigin)
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+            ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept')
+            ->withHeader('Access-Control-Max-Age', '86400');
+        return $response;
+    }
+
+    $response = $handler->handle($request);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', $responseOrigin)
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+        ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
+});
+
 $app->addBodyParsingMiddleware();
 $app->addErrorMiddleware(true, true, true);
 
